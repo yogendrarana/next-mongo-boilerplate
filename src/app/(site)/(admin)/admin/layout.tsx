c1a9d5a4965996ed37a9bb58099e74bf062f471c
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-11-16 23:18:16 +0545

    chore

--- src/app/(site)/(admin)/admin/layout.tsx
+++ src/app/(site)/(admin)/admin/layout.tsx
@@ -6,9 +6,6 @@ import { SiteHeader } from "@/components/layout/site-header";
 import { DashboardSidebar } from "../_components/dashboard-sidebar";
 
 interface DashboardStoreLayoutProps {
-    params: {
-        storeId: string;
-    };
     children: React.ReactNode;
 }
 
@@ -24,10 +21,8 @@ export default async function DashboardStoreLayout({ children }: DashboardStoreL
             <SiteHeader />
             <Shell>
                 <div className="flex py-4 md:py-10">
-                    <DashboardSidebar className="h-full w-[15rem] hidden lg:block" />
-                    <div className="w-full">
-                        <main className="w-full">{children}</main>
-                    </div>
+                    <DashboardSidebar className="h-full sticky top-24 w-[15rem] hidden lg:block" />
+                    <main className="w-full">{children}</main>
                 </div>
             </Shell>
         </div>
